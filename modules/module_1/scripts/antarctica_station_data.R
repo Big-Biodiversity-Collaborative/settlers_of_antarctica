177bfaa9d32604007d6a58134c246d22ab5134e7
keatonwilson <keatonwilson@me.com>
2019-11-07 14:55:16 -0700

    Added raw and aggregated antarctica station data

---
+++ modules/module_1/scripts/antarctica_station_data.R
@@ -0,0 +1,43 @@
+# Antarctica Station Data
+# Keaton Wilson
+# keatonwilson@me.com
+# 2019-11-7
+
+# packages
+library(tidyverse)
+library(lubridate)
+
+
+# Multiple stations
+test = read_delim("./modules/module_1/data/raw_antarctica_station_data/ag4201801q3h.txt", 
+                  delim = " ", 
+                  skip = 2, 
+                  col_names = c("year", "day", "month", 
+                                "running_day", "hour", 
+                                "temp", "pressure", 
+                                "wind_speed", "wind_direction", 
+                                "humidity", "delta_t"))
+
+test %>%
+  mutate(date = paste(test$year, test$month, test$day, sep = "-") %>% ymd()) %>%
+  mutate_if(is.character, as.numeric)
+
+
+master_station_data = data.frame()
+files = list.files("./modules/module_1/data/raw_antarctica_station_data/", 
+                   full.names = TRUE)
+for(i in 1:length(files)) {
+  temp = read_delim(files[i], 
+                    delim = " ", 
+                    skip = 2, 
+                    col_names = c("year", "day", "month", 
+                                  "running_day", "hour", 
+                                  "temp", "pressure", 
+                                  "wind_speed", "wind_direction", 
+                                  "humidity", "delta_t"))
+  temp = temp %>%
+    mutate(date = paste(test$year, test$month, test$day, sep = "-") %>% ymd()) %>%
+    mutate_if(is.character, as.numeric)
+  master_station_data = bind_cols(master_station_data, temp)
+}
+}
