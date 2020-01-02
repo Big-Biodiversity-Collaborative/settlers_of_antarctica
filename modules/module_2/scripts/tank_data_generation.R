25344f91f3947b31dd0132480315c548a9197fd0
keatonwilson <keatonwilson@me.com>
2020-01-02 12:24:07 -0700

    Modified tank-data-generation script.

--- modules/module_2/scripts/tank_data_generation.R
+++ modules/module_2/scripts/tank_data_generation.R
@@ -5,6 +5,7 @@
 
 # packages
 library(tidyverse)
+library(truncnorm)
 
 # generating tank data frame
 fish_tanks = tibble(tank_id = c(1:1000), 
@@ -20,7 +21,15 @@ fish_tanks = tibble(tank_id = c(1:1000),
                           round(rnorm(n = 250, mean = 12, sd = 1))), 
         tank_volume = ifelse(species == "tilapia", 
                           (rnorm(n = 750, mean = 400, sd = 0.8)), 
-                          (rnorm(n = 250, mean = 400, sd = 0.8))))
+                          (rnorm(n = 250, mean = 400, sd = 0.8))),
+        num_sick = ifelse(species == "tilapia",
+                          round(rtruncnorm(750, a = 0, mean = 2, sd = 5)),
+                          round(rtruncnorm(750, a = 0, mean = 10, sd = 5))))
   
+# Looking at data
+fish_tanks %>%
+  ggplot(aes(x = num_sick, fill = factor(species))) +
+  geom_histogram(binwidth = 1)
 
-
+# writing to csv
+write_csv(fish_tanks, file = "./modules/module_2/data/fish_tank_data.csv")
